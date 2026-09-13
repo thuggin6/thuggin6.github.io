a0fd1d4f33c942edaa766335e2f05f1dab823938
Clemson Alumni <thuggin6@email.cpcc.edu>
2026-09-13 00:28:22 -0400

    Update script.js

--- web140/script.js
+++ web140/script.js
@@ -1,1 +1,18 @@
-
+const button = document.getElementById('welcomeButton');
+const message = document.querySelector('#message');
+      
+// Add a click event listener 
+button.addEventListener('click', function() {
+  if(message.textContent.trim().length == 0){
+    button.textContent = 'GoodBye!';
+    // Change color of button when clicked
+    button.style.backgroundColor = '#28a745';
+    message.textContent = "Thanks for reading my portfolio!";
+  }
+  else{
+    message.textContent = "";
+    button.style.backgroundColor = '#222';
+    button.textContent = 'Hello!';
+  }
+            
+});
